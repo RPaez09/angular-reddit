c77b91282fc9bae20818c83893da2a6f5d4affd5
Richard Paez <Richardp809@gmail.com>
2017-05-17 23:17:46 -0400

    Upvoting and downvoting a sample article component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
-    UserItemComponent
+    UserItemComponent,
+    UserListComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
--- src/app/user-item/user-item.component.ts
+++ src/app/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'user-item',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserItemComponent implements OnInit {
 
-    Myname = 'User-Item Component'
+    @Input() name:string; // the name property added to the UserItemComponent class
 
-    constructor(){ }
+    constructor(){
+        
+     }
 
     ngOnInit(){
 
